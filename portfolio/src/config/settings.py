e7e890c4ba7dc7de95bfd541df610eff893b47c1
Roman <iamromandev@gmail.com>
2023-03-26 02:03:15 +0600

    update settings and config and env

--- portfolio/src/config/settings.py
+++ portfolio/src/config/settings.py
@@ -10,11 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+import environ
+from loguru import logger
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+root = environ.Path(__file__) - 3  # get root of the project
+
+env = environ.Env()
+env.read_env(BASE_DIR.joinpath('.env'))  # reading .env file
+
+logger.debug(f"ProcessId {os.getpid()}")
+logger.debug(f"BaseDir: {BASE_DIR} {type(BASE_DIR)}")
+logger.debug(f"RootDir: {root}")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/

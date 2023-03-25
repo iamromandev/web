1180448d93fe580c8c198a03cdbde84908831881
Roman <iamromandev@gmail.com>
2023-03-26 05:09:27 +0600

    mysql setup

--- portfolio/src/config/settings.py
+++ portfolio/src/config/settings.py
@@ -90,8 +90,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db.portfolio',
+        'USER': 'local',
+        'PASSWORD': 'local',
+        'HOST': 'db',  # db for container name; 127.0.0.1 for terminal
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8',
+            'use_unicode': True,
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 

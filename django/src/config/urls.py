41557927d30d238c1ac0f87cb50cd51564dcf7cf
Roman <iamromandev@gmail.com>
2023-04-02 02:28:33 +0600

    minor update on template

--- django/src/config/urls.py
+++ django/src/config/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include('django_browser_reload.urls')),
+    path('__reload__/', include('django_browser_reload.urls')),
     path('', include('apps.home.urls')),
+    #path('', TemplateView.as_view(template_name='base.html')),
 ]

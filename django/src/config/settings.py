d3dd9b8d53cec1eecc2d07a2a1c4d3a2911f8656
Roman <iamromandev@gmail.com>
2023-04-12 04:10:35 +0600

    static working 2

--- django/src/config/settings.py
+++ django/src/config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'src/static/'
-STATIC_ROOT = BASE_DIR.joinpath('src/staticfiles') if DEBUG else '/home/iamromandev/public_html/staticfiles'
+STATIC_ROOT = BASE_DIR.joinpath('src/staticfiles')  # if DEBUG else '/home/iamromandev/public_html/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

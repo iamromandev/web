61a29a6f1925393c880b06ce012dcdaced0ab98c
Roman <iamromandev@gmail.com>
2023-05-09 22:52:07 +0600

    test deps

--- django/src/config/settings.py
+++ django/src/config/settings.py
@@ -191,7 +191,7 @@ AUTH_USER_MODEL = 'core.User'
 logger.remove()
 
 logger.add(
-    sys.stdout.reconfigure(encoding="utf-8", errors="backslashreplace"),
+    sys.stderr,
     format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{"
     "function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
     backtrace=True,

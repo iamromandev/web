b4412aed228f3eb17e996d096eeed60bce0216dd
Roman <iamromandev@gmail.com>
2023-05-26 05:12:28 +0600

    enable corsheaders

--- django/src/config/settings.py
+++ django/src/config/settings.py
@@ -76,9 +76,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",  # higher stack
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",  # higher stack
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",

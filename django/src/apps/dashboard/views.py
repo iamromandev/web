5dbf811848c4a905a270ffba86436c7fd7c56b6f
Roman <iamromandev@gmail.com>
2023-06-05 09:14:33 +0600

    enabled dashboard

--- django/src/apps/dashboard/views.py
+++ django/src/apps/dashboard/views.py
@@ -4,35 +4,36 @@ from django.http import HttpResponse
 
 from apps.core.models import State
 from apps.dictionary.enums import (
-    Type,
-    Subtype,
+    Type as DictionaryType,
+    Subtype as DictionarySubtype,
+    Origin as DictionaryOrigin,
     State as DictionaryState,
 )
 
-# from apps.dictionary.models import (
-#     Word,
-#     Pronunciation,
-#     Definition,
-#     Example,
-# )
+from apps.dictionary.models import (
+    Word,
+    Pronunciation,
+    Definition,
+    Example,
+)
 
 
 # Create your views here.
 
 
 def dictionary_details(request):
-    # synced_words = Store.objects.filter(
-    #     type=Type.WORD.name, subtype=Subtype.DEFAULT.name, state=State.SYNCED.name
-    # ).count()
-    # words = Word.objects.count()
-    # pronunciations = Pronunciation.objects.count()
-    # definitions = Definition.objects.count()
-    # examples = Example.objects.count()
-    # data = {
-    #     "synced_words": synced_words,
-    #     "words": words,
-    #     "pronunciations": pronunciations,
-    #     "definitions": definitions,
-    #     "examples": examples,
-    # }
-    return HttpResponse(json.dumps({}, sort_keys=False, indent=4))
+    synced_words = State.objects.filter(
+        type=DictionaryType.WORD.value, subtype=DictionarySubtype.DEFAULT.value, state=State.SYNCED.value
+    ).count()
+    words = Word.objects.count()
+    pronunciations = Pronunciation.objects.count()
+    definitions = Definition.objects.count()
+    examples = Example.objects.count()
+    data = {
+        "synced_words": synced_words,
+        "words": words,
+        "pronunciations": pronunciations,
+        "definitions": definitions,
+        "examples": examples,
+    }
+    return HttpResponse(json.dumps(data, sort_keys=False, indent=4))

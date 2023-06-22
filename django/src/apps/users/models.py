ec7ddfe2e76b5a138e54540e7e5d08e65b06eb76
Roman <iamromandev@gmail.com>
2023-06-22 07:47:43 +0600

    halt of profile model

--- django/src/apps/users/models.py
+++ django/src/apps/users/models.py
@@ -6,6 +6,7 @@ from django.utils.translation import gettext_lazy as _
 from django_softdelete.models import SoftDeleteModel
 
 from apps.core.models import Source, User
+from apps.media.models import Image
 
 
 # Create your models here.
@@ -100,10 +101,12 @@ class Address(SoftDeleteModel):
 #     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
 #     source = models.ForeignKey(Source, related_name="users_profiles", on_delete=models.DO_NOTHING)
 #     user = models.ForeignKey(User, related_name="profiles", on_delete=models.DO_NOTHING)
+#     #photo = models.OneToOneField(Image, on_delete=models.CASCADE)
+#     photo = models.ImageField(upload_to ='uploads/')
 #     gender = models.CharField(max_length=16, choices=Gender.choices, default=None, blank=True, null=True)
 #     birthday = models.DateField(blank=True, null=True)
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now=True)
 #
 #     class Meta:
-#         ordering = ("locality", "street")
+#         ordering = ("user",)

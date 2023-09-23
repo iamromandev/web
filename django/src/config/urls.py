31d481879e58e8ede12969b45c85947456695505
Roman <iamromandev@gmail.com>
2023-09-23 06:21:10 +0600

    web+django - user registration

--- django/src/config/urls.py
+++ django/src/config/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     # path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     # path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     # path("admin/", admin.site.urls),
-    path("auths/", include("apps.auths.urls")),
+    # path("auths/", include("apps.auths.urls")),
     path("", include("apps.bio.urls")),
     path("dashboard/", include("apps.dashboard.urls")),
     path("dictionary/", include("apps.dictionary.urls")),

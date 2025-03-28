ab47cda07e87593e841f25aea89d8a4ea2d3fd5b
Roman <iamromandev@gmail.com>
2025-03-28 12:15:52 +0600

    feat/page - added ruff, model code and location, update gh ci
    * added authn + account
    
    * added ruff in makefile
    
    * ruff configure update 1
    
    * ruff configure update 2
    
    * refactor
    
    * github workflow linting update for page project
    
    * ruff check only
    
    * update ruff config for DJ001
    
    * ruff check fixed
    
    * update ruff rules
    
    * remove django cl
    
    * reformat code
    
    * added account
    
    * basic linting added
    
    * gh ci in only pr
    
    * update pip on page-ci
    
    * update toml
    
    * update toml
    
    * ruff refactor for isort
    
    * added location model

---
+++ page/src/apps/account/models.py
@@ -0,0 +1,36 @@
+import uuid
+
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_softdelete.models import SoftDeleteModel
+from phonenumber_field.modelfields import PhoneNumberField
+
+from apps.core.models import User
+
+
+# class Profile(SoftDeleteModel):
+#     class Gender(models.TextChoices):
+#         MALE = 'male', _('Male')
+#         FEMALE = 'female', _('Female')
+#         NON_BINARY = 'non_binary', _('Non-binary')
+#         GENDER_FLUID = 'gender_fluid', _('Gender Fluid')
+#         AGENDER = 'agender', _('Agender')
+#         PREFER_NOT_TO_SAY = 'prefer_not_to_say', _('Prefer not to say')
+#
+#     user = models.OneToOneField(
+#         User,
+#         on_delete=models.CASCADE,
+#         related_name="profile",
+#         help_text="The associated Django User"
+#     )
+#     gender = models.CharField(max_length=32, choices=Gender.choices, blank=True, null=True)
+#     name = models.CharField(max_length=128, blank=True, null=True)
+#     bio = models.TextField(blank=True, null=True)
+#     birthdate = models.DateField(blank=True, null=True)
+#     phone = PhoneNumberField(blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     class Meta:
+#         ordering = ["name"]

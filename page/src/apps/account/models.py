c930e92cd1b4ca817d348a0265a434dff92e9e19
Roman <iamromandev@gmail.com>
2025-03-28 20:34:51 +0600

    refactor in language and location

--- page/src/apps/account/models.py
+++ page/src/apps/account/models.py
@@ -6,31 +6,33 @@ from django.utils.translation import gettext_lazy as _
 from django_softdelete.models import SoftDeleteModel
 from phonenumber_field.modelfields import PhoneNumberField
 
-from apps.core.models import User
+from apps.core.models import Code, Language, Location, User
 
 
-# class Profile(SoftDeleteModel):
-#     class Gender(models.TextChoices):
-#         MALE = 'male', _('Male')
-#         FEMALE = 'female', _('Female')
-#         NON_BINARY = 'non_binary', _('Non-binary')
-#         GENDER_FLUID = 'gender_fluid', _('Gender Fluid')
-#         AGENDER = 'agender', _('Agender')
-#         PREFER_NOT_TO_SAY = 'prefer_not_to_say', _('Prefer not to say')
-#
-#     user = models.OneToOneField(
-#         User,
-#         on_delete=models.CASCADE,
-#         related_name="profile",
-#         help_text="The associated Django User"
-#     )
-#     gender = models.CharField(max_length=32, choices=Gender.choices, blank=True, null=True)
-#     name = models.CharField(max_length=128, blank=True, null=True)
-#     bio = models.TextField(blank=True, null=True)
-#     birthdate = models.DateField(blank=True, null=True)
-#     phone = PhoneNumberField(blank=True, null=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#
-#     class Meta:
-#         ordering = ["name"]
+class Profile(SoftDeleteModel):
+    class Gender(models.TextChoices):
+        MALE = 'male', _('Male')
+        FEMALE = 'female', _('Female')
+        NON_BINARY = 'non_binary', _('Non-binary')
+        GENDER_FLUID = 'gender_fluid', _('Gender Fluid')
+        AGENDER = 'agender', _('Agender')
+        PREFER_NOT_TO_SAY = 'prefer_not_to_say', _('Prefer not to say')
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile",
+        help_text="The associated Django User"
+    )
+    gender = models.CharField(max_length=32, choices=Gender.choices, blank=True, null=True)
+    name = models.CharField(max_length=128, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    birthdate = models.DateField(blank=True, null=True)
+    phone = PhoneNumberField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = _("profile")
+        verbose_name_plural = _("profiles")

df64d3724ba06342e7936caea1e8143e012dfc4b
Roman <iamromandev@gmail.com>
2025-04-04 17:53:15 +0600

    refactor TextChoices

--- page/src/apps/account/models.py
+++ page/src/apps/account/models.py
@@ -6,9 +6,9 @@ from django.utils.translation import gettext_lazy as _
 from django_softdelete.models import SoftDeleteModel
 from phonenumber_field.modelfields import PhoneNumberField
 
-#from apps.core.models import Code, Language, User
-
-
+# from apps.core.models import Code, Language, User
+#
+#
 # class Profile(SoftDeleteModel):
 #     class Gender(models.TextChoices):
 #         MALE = 'male', _('Male')
@@ -17,12 +17,12 @@ from phonenumber_field.modelfields import PhoneNumberField
 #         GENDER_FLUID = 'gender_fluid', _('Gender Fluid')
 #         AGENDER = 'agender', _('Agender')
 #         PREFER_NOT_TO_SAY = 'prefer_not_to_say', _('Prefer not to say')
+#         OTHER = 'other', _('Other')
 #
 #     user = models.OneToOneField(
 #         User,
-#         on_delete=models.CASCADE,
-#         related_name="profile",
-#         help_text="The associated Django User"
+#         on_delete=models.SET_NULL,
+#         related_name="profile"
 #     )
 #     display_name = models.CharField(max_length=128, blank=True, null=True)
 #     gender = models.CharField(max_length=32, choices=Gender.choices, blank=True, null=True)
@@ -34,5 +34,5 @@ from phonenumber_field.modelfields import PhoneNumberField
 #
 #     class Meta:
 #         ordering = ["display_name"]
-#         verbose_name = _("profile")
-#         verbose_name_plural = _("profiles")
+#         verbose_name = _("Profile")
+#         verbose_name_plural = _("Profiles")

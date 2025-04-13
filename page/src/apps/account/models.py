e5dd2426d2e374ce71e1fe9fec5c1501802893c7
Roman <iamromandev@gmail.com>
2025-04-13 23:35:03 +0600

    refactor core models

--- page/src/apps/account/models.py
+++ page/src/apps/account/models.py
@@ -8,40 +8,40 @@ from django_softdelete.models import SoftDeleteModel
 from phonenumber_field.modelfields import PhoneNumberField
 from timezone_field import TimeZoneField
 
-from apps.core.models import Address, Code, Language, Platform, Url, User
-from apps.core.time_utils import TimezoneChoices
-
-
-class SocialPlatform(SoftDeleteModel):
-    class Type(models.TextChoices):
-        FACEBOOK = 'facebook', _('Facebook')
-        YOUTUBE = 'youtube', _('YouTube')
-        WHATSAPP = 'whatsapp', _('WhatsApp')
-        INSTAGRAM = 'instagram', _('Instagram')
-        TIKTOK = 'tiktok', _('TikTok')
-        WECHAT = 'wechat', _('WeChat')
-        FACEBOOK_MESSENGER = 'facebook_messenger', _('Facebook Messenger')
-        TELEGRAM = 'telegram', _('Telegram')
-        TWITTER = 'twitter', _('X (formerly Twitter)')
-        SNAPCHAT = 'snapchat', _('Snapchat')
-        PINTEREST = 'pinterest', _('Pinterest')
-        LINKEDIN = 'linkedin', _('LinkedIn')
-        REDDIT = 'reddit', _('Reddit')
-        DISCORD = 'discord', _('Discord')
-        TUMBLR = 'tumblr', _('Tumblr')
-        THREADS = 'threads', _('Threads')
-        BLUESKY = 'bluesky', _('Bluesky')
-        MASTODON = 'mastodon', _('Mastodon')
-        OTHER = "other", _("Other")
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    platform = models.OneToOneField(
-        Platform, on_delete=models.SET_NULL, related_name="social_platform", blank=True, null=True
-    )
-    type = models.CharField(max_length=32, choices=Type.choices, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+# from apps.core.models import Address, Code, Language, Platform, Url, User
+# from apps.core.time_utils import TimezoneChoices
+#
+#
+# class SocialPlatform(SoftDeleteModel):
+#     class Type(models.TextChoices):
+#         FACEBOOK = 'facebook', _('Facebook')
+#         YOUTUBE = 'youtube', _('YouTube')
+#         WHATSAPP = 'whatsapp', _('WhatsApp')
+#         INSTAGRAM = 'instagram', _('Instagram')
+#         TIKTOK = 'tiktok', _('TikTok')
+#         WECHAT = 'wechat', _('WeChat')
+#         FACEBOOK_MESSENGER = 'facebook_messenger', _('Facebook Messenger')
+#         TELEGRAM = 'telegram', _('Telegram')
+#         TWITTER = 'twitter', _('X (formerly Twitter)')
+#         SNAPCHAT = 'snapchat', _('Snapchat')
+#         PINTEREST = 'pinterest', _('Pinterest')
+#         LINKEDIN = 'linkedin', _('LinkedIn')
+#         REDDIT = 'reddit', _('Reddit')
+#         DISCORD = 'discord', _('Discord')
+#         TUMBLR = 'tumblr', _('Tumblr')
+#         THREADS = 'threads', _('Threads')
+#         BLUESKY = 'bluesky', _('Bluesky')
+#         MASTODON = 'mastodon', _('Mastodon')
+#         OTHER = "other", _("Other")
+#
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     platform = models.OneToOneField(
+#         Platform, on_delete=models.SET_NULL, related_name="social_platform", blank=True, null=True
+#     )
+#     type = models.CharField(max_length=32, choices=Type.choices, blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
 # class Profile(SoftDeleteModel):
 #     class Gender(models.TextChoices):
 #         MALE = 'male', _('Male')
@@ -58,7 +58,9 @@ class SocialPlatform(SoftDeleteModel):
 #         PENDING = 'pending', _('Pending')
 #         SUSPENDED = 'suspended', _('Suspended')
 #         BLOCKED = 'blocked', _('Blocked')
+#         OTHER = 'other', _('Other')
 #
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 #     user = models.OneToOneField(
 #         User,
 #         on_delete=models.SET_NULL,
@@ -66,7 +68,6 @@ class SocialPlatform(SoftDeleteModel):
 #         blank=True,
 #         null=True,
 #     )
-#
 #     display_name = models.CharField(max_length=128, blank=True, null=True)
 #     avatar = AvatarField(upload_to='profile_avatars/', blank=True, null=True)
 #     photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
@@ -77,7 +78,13 @@ class SocialPlatform(SoftDeleteModel):
 #     birthdate = models.DateField(blank=True, null=True)
 #
 #     phone = PhoneNumberField(blank=True, null=True)
-#     website = models.URLField(blank=True, null=True)
+#     website = models.OneToOneField(
+#         Url,
+#         on_delete=models.SET_NULL,
+#         related_name="profile",
+#         blank=True,
+#         null=True,
+#     )
 #
 #     languages = models.ManyToManyField(Language, related_name="profiles", blank=True)
 #     current_address = models.ForeignKey(

1de6c499bc741980aebba5aba48c3920e294840d
Roman <iamromandev@gmail.com>
2025-04-20 00:58:36 +0600

    created profile repo

---
+++ page/src/apps/account/repos/profile_repo.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from apps.account.models import Profile
+from apps.core.models import User
+from apps.core.repos.base_repo import BaseRepo
+
+
+class ProfileRepo(BaseRepo[Profile]):
+
+    def __init__(self):
+        super().__init__(Profile)
+
+    def get_profile_by_user(self, user: User) -> Optional[Profile]:
+        try:
+            profile = Profile.objects.get(user=user)
+            return profile
+        except ObjectDoesNotExist:
+            return None

392c280e900a3446716e6c1734f7f76665f26dc0
Roman <iamromandev@gmail.com>
2025-04-19 15:02:43 +0600

    fix imp

--- page/passenger_wsgi.py
+++ page/passenger_wsgi.py
@@ -1,8 +1,8 @@
-import imp
 import os
 import sys
+from importlib.machinery import SourceFileLoader
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-wsgi = imp.load_source("wsgi", "src/passenger_wsgi.py")
+wsgi = SourceFileLoader("wsgi", "src/passenger_wsgi.py").load_module()
 application = wsgi.application

8e73ab5b9bf8e8c60c68e41a76384c1e53ac4f83
Roman <iamromandev@gmail.com>
2025-04-22 02:21:29 +0600

    refactor profile time zone

--- page/src/apps/core/utils/time_utils.py
+++ page/src/apps/core/utils/time_utils.py
@@ -1,24 +1,25 @@
 from datetime import datetime
 from functools import lru_cache
-from zoneinfo import ZoneInfo, available_timezones
+
+import pytz
 
 
 class TimezoneChoices:
     @classmethod
     @lru_cache(maxsize=1)  # Cache the result with a size of 1 since it won't change
-    def get_choices(cls):
+    def get_choices(cls) -> list[tuple[str, str]]:
         """
-            Generate a list of timezone choices with UTC offsets.
-            Cached using lru_cache for performance.
-            """
+        Generate a list of timezone choices with UTC offsets.
+        Cached using lru_cache for performance.
+        """
         now = datetime.now()
         choices = []
-        for tz_name in sorted(available_timezones()):
-            tz = ZoneInfo(tz_name)
-            offset = now.replace(tzinfo=tz).utcoffset()
+        for tz_name in pytz.common_timezones:
+            tz = pytz.timezone(tz_name)
+            offset = tz.utcoffset(now)
             if offset is not None:
-                offset_hours = offset.total_seconds() / 3600
+                offset_seconds = offset.total_seconds()
+                offset_hours = offset_seconds / 3600
                 label = f"{tz_name} (UTC{offset_hours:+.1f})"
                 choices.append((tz_name, label))
         return choices
-

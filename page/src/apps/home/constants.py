ca2bf0dda8044c9af874b7c4dbb0bd8093cdab62
Roman <iamromandev@gmail.com>
2025-04-25 17:05:31 +0600

    page - added user bio info into conststans

---
+++ page/src/apps/home/constants.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+USER: dict[str, Any] = {
+    "intro": "Hi, I am Roman",
+    "title": "Software Engineer | Python, Java, Kotlin, JavaScript | Backend & Android Developer | Django, FastAPI, Spring | Docker & AWS",
+    "focus": "Currently, I am focused on Backend development, DevOps and AI practicing.",
+    'bio': "A software engineer with a strong focus on building scalable, reliable applications and continuously exploring emerging technologies.",
+    "about": "I am passionate Software Engineer with hands-on experience in Python, Java, Kotlin, and JavaScript. I specialize in building scalable backend services and mobile applications using frameworks like Django, FastAPI, Spring, and Android. I’m also well-versed in Docker and AWS, and always excited to explore new technologies and solve real-world problems through clean, efficient code.",
+    "current_designation": "Senior Software Engineer",
+    "current_company": "Safara | safara.com",
+}

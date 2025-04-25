700ac4349f521abf460e51c93526de36fbbe703c
Roman <iamromandev@gmail.com>
2025-04-25 17:05:31 +0600

    page - added user bio info into conststans

--- page/src/apps/home/views.py
+++ page/src/apps/home/views.py
@@ -1,5 +1,16 @@
+from typing import Any
+
 from django.views.generic import TemplateView
+
+from .constants import (
+    USER,
+)
 
 
 class IndexView(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["user"] = USER
+        return context

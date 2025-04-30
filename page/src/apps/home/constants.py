0e7ae960a49c84d4212cfa1bdb8cb74b789938fa
Roman <iamromandev@gmail.com>
2025-04-30 08:36:54 +0600

    update bio

--- page/src/apps/home/constants.py
+++ page/src/apps/home/constants.py
@@ -1,10 +1,10 @@
 from typing import Any
 
 USER: dict[str, Any] = {
-    "intro": "Hi, I am Roman",
-    "title": "Software Engineer | Python, Java, Kotlin, JavaScript | Backend & Android Developer | Django, FastAPI, Spring | Docker & AWS",
-    "focus": "Currently, I am focused on Backend development, DevOps and AI practicing.",
-    'bio': "A software engineer with a strong focus on building scalable, reliable applications and continuously exploring emerging technologies.",
+    "intro": "Hi there 👋 I'm Roman",
+    "title": "Software Enthusiast || Python | Java | Kotlin | JavaScript || Backend & Android Developer || Django | FastAPI | Spring Boot || Docker & AWS",
+    "focus": "Currently, I am focused on Backend development, DevOps and AI",
+    'bio': "Software Enthusiast | Python, Java, Kotlin, JavaScript | Backend Development, DevOps & AI | Building Scalable Apps with Django, FastAPI, Spring, and Android",
     "about": "I am passionate Software Engineer with hands-on experience in Python, Java, Kotlin, and JavaScript. I specialize in building scalable backend services and mobile applications using frameworks like Django, FastAPI, Spring, and Android. I’m also well-versed in Docker and AWS, and always excited to explore new technologies and solve real-world problems through clean, efficient code.",
     "current_designation": "Senior Software Engineer",
     "current_company": "Safara | safara.com",
